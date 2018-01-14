2d696791157e8771fddb35899f25c0c0c945dac6
shanchao <shanchao@wosai-inc.com>
2018-01-14 10:45:57 +0800

    hello world

---
+++ spring-boot-helloworld/src/main/java/org/schhx/springbootlearn/controller/HelloworldController.java
@@ -0,0 +1,14 @@
+package org.schhx.springbootlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name){
+        return String.format("Hello, %s !", name);
+    }
+}
