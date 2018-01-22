37bd3d0c772e0422e92f8669926c3dcf571e7a9a
shanchao <shanchao@wosai-inc.com>
2018-01-22 10:17:51 +0800

    一些优化

---
+++ spring-boot-exception-handler/src/main/java/org/schhx/springbootlearn/exception/handler/MyErrorController.java
@@ -0,0 +1,20 @@
+package org.schhx.springbootlearn.exception.handler;
+
+import org.schhx.springbootlearn.vo.ErrorVO;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyErrorController implements ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public ErrorVO error(){
+        return new ErrorVO("兜底");
+    }
+}
---
+++ spring-boot-log/src/test/java/org/schhx/springbootlearn/LogTest.java
@@ -0,0 +1,24 @@
+package org.schhx.springbootlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+    @Test
+    public void printLog() throws Exception {
+        logger.trace("msg:{}", "trace");
+        logger.debug("msg:{}", "debug");
+        logger.info("msg:{}", "info");
+        logger.warn("msg:{}", "warn");
+        logger.error("msg:{}", "error");
+    }
+}
