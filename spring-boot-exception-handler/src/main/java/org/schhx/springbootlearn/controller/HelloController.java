afdd2b454a1cb09728bf1469df8cf2b490fd8203
shanchao <shanchao@wosai-inc.com>
2018-04-18 16:04:42 +0800

    update

--- spring-boot-exception-handler/src/main/java/org/schhx/springbootlearn/controller/HelloController.java
+++ spring-boot-exception-handler/src/main/java/org/schhx/springbootlearn/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/baseException")
     public void baseException(){
-        throw new NotFoundException("NotFoundException");
+        throw new NotFoundException("对象不存在");
     }
 
     @GetMapping("/exception")
--- spring-boot-exception-handler/src/main/java/org/schhx/springbootlearn/exception/handler/MyErrorController.java
+++ spring-boot-exception-handler/src/main/java/org/schhx/springbootlearn/exception/handler/MyErrorController.java
@@ -1,12 +1,21 @@
 package org.schhx.springbootlearn.exception.handler;
 
 import org.schhx.springbootlearn.vo.ErrorVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MyErrorController implements ErrorController {
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
 
     @Override
     public String getErrorPath() {
@@ -14,7 +23,9 @@ public class MyErrorController implements ErrorController {
     }
 
     @RequestMapping("/error")
-    public ErrorVO error(){
-        return ErrorVO.of("兜底");
+    public ErrorVO error(HttpServletRequest request){
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        Throwable throwable = errorAttributes.getError(requestAttributes);
+        return ErrorVO.of(throwable.getMessage());
     }
 }
